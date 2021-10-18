b9a900c5a37b95bbb4204622acac7e7d56836b64
Sergey Gomzyakov <surreios@gmail.com>
2021-10-18 14:47:40 +0500

    Distinct aggregation added

--- drf_aggregation/enums.py
+++ drf_aggregation/enums.py
@@ -5,6 +5,7 @@ from django.utils.translation import gettext_lazy as _
 class Aggregation(models.TextChoices):
     AVERAGE = "average", _("average")
     COUNT = "count", _("count")
+    DISTINCT = "distinct", _("distinct")
     MAX = "maximum", _("maximum")
     MIN = "minimum", _("minimum")
     PERCENT = "percent", _("percent")
