743ff7dda26ac0cad2e1411a77a0e02410d7b38c
Nikita B <nikita.balobanov@gmail.com>
2024-10-04 16:15:33 +0300

    add some tests with multiple aggregations

--- tests/test_data.py
+++ tests/test_data.py
@@ -128,6 +128,25 @@ ANNOTATIONS_TESTING = [
         },
         {"value_numerator": 3, "value_denominator": 6, "value": 0.5},
     ),
+    (
+        {
+            "aggregations": {
+                "count": {"type": "count"},
+                "percent": {
+                    "type": "percent",
+                    "additional_filter": {
+                        "type": "operator",
+                        "data": {
+                            "attribute": "group2",
+                            "operator": "=",
+                            "value": "1",
+                        },
+                    },
+                },
+            }
+        },
+        {"count": 6, "percent_numerator": 3, "percent_denominator": 6, "percent": 0.5},
+    ),
 ]
 
 UNSORTED_GROUPS_TESTING = [
@@ -147,6 +166,21 @@ UNSORTED_GROUPS_TESTING = [
             {"group1": "3", "value": 3},
         ],
     ),
+    # GROUP BY ONE FIELD WITH MULTIPLE AGGREGATION
+    (
+        {
+            "aggregations": {
+                "cnt": {"type": "count"},
+                "mx": {"type": "maximum", "field": "integer"},
+            },
+            "group_by": "group1",
+        },
+        [
+            {"group1": "1", "cnt": 2, "mx": 2},
+            {"group1": "2", "cnt": 1, "mx": 0},
+            {"group1": "3", "cnt": 3, "mx": 5},
+        ],
+    ),
     # GROUP BY MULTIPLE FIELDS
     (
         {
@@ -164,6 +198,24 @@ UNSORTED_GROUPS_TESTING = [
             {"group1": "2", "group2": "1", "value": 1},
             {"group1": "3", "group2": "1", "value": 1},
             {"group1": "1", "group2": "1", "value": 1},
+        ],
+    ),
+    # GROUP BY MULTIPLE FIELDS WITH MULTIPLE AGGREGATION
+    (
+        {
+            "aggregations": {
+                "cnt": {"type": "count"},
+                "mx": {"type": "maximum", "field": "integer"},
+            },
+            "group_by": "group1,group2",
+        },
+        [
+            {"group1": "3", "group2": "3", "cnt": 1, "mx": 5},
+            {"group1": "3", "group2": "2", "cnt": 1, "mx": 3},
+            {"group1": "1", "group2": "2", "cnt": 1, "mx": 1},
+            {"group1": "2", "group2": "1", "cnt": 1, "mx": 0},
+            {"group1": "3", "group2": "1", "cnt": 1, "mx": 4},
+            {"group1": "1", "group2": "1", "cnt": 1, "mx": 2},
         ],
     ),
     # IGNORE LIMIT BY AND SHOW OTHER WITHOUT LIMIT
@@ -258,6 +310,28 @@ SORTED_GROUPS_TESTING = [
             {"group1": "1", "group2": "2", "value": 1},
             {"group1": "1", "group2": "1", "value": 1},
             {"group1": "2", "group2": "1", "value": 1},
+        ],
+    ),
+    # GROUP BY MULTIPLE FIELDS WITH SORT BY TOTAL GROUP VALUE WITH MULTIPLE AGGREGATION
+    (
+        {
+            "aggregations": {
+                "value": {
+                    "type": "count",
+                    "index_by_group": "group1",
+                },
+                "mx": {"type": "maximum", "field": "integer"},
+            },
+            "group_by": "group1,group2",
+            "order_by": "-group1__value,-group2",
+        },
+        [
+            {"group1": "3", "group2": "3", "value": 1, "mx": 5},
+            {"group1": "3", "group2": "2", "value": 1, "mx": 3},
+            {"group1": "3", "group2": "1", "value": 1, "mx": 4},
+            {"group1": "1", "group2": "2", "value": 1, "mx": 1},
+            {"group1": "1", "group2": "1", "value": 1, "mx": 2},
+            {"group1": "2", "group2": "1", "value": 1, "mx": 0},
         ],
     ),
     # LIMIT NUMBER OF RETURNED GROUPS
